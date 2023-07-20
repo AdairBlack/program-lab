9a9215080a7a6f276f83c64206f08b3274f810c2
edgar <eternaledgar@qq.com>
2023-07-20 20:58:20 +0800

    reorganize

---
+++ python-test/d2l/main.py
@@ -0,0 +1,18 @@
+message = "What the fuck"
+
+firstName = "Edgar"
+secondName = "Adair"
+
+fullName = f"{firstName}.{secondName}"
+
+myList = ['computer', 'math', 'deeplearning', 'python']
+
+
+# python test
+print(message)
+print(fullName)
+print(myList)
+
+
+for item in myList:
+    print(item)
